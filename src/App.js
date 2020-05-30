1c06199609d7dda0657290c2e8b41b496e0f0c84
Saurabh Gupta <saurabhgupta0508@gmail.com>
2020-05-30 15:35:44 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
